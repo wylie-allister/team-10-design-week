456138c2fed7152c318e8f1b40e560b764011ef4
Myota <allister.wylie@mohawkcollege.ca>
2025-10-06 20:59:48 -0400

    Add first person maze code wip

--- Team10BatGame/Maze.cs
+++ Team10BatGame/Maze.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cff8a474e70c280ce2408fe295cfefe36d5eb880b8143fdc07fe7796bc9d4fd6
-size 1861
+oid sha256:9ad983398e1f48c50da54e2455ce29a8c082dd16c33832e58b4e5a5343ce9d79
+size 3178
--- Team10BatGame/Project.cs
+++ Team10BatGame/Project.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7a648ce99d1c7204a6f87836656c4dc279e4934a2c41229a0b5ca691241bd1e
-size 462
+oid sha256:cbde1bf90d89c378b205388bbf6ed5f0264033aac4235f3db716b13f80ed7d7f
+size 392
